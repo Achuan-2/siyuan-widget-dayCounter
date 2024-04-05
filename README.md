99a8912896a39c3a8b0f78bbe409c03537f4cadc
Tron <achuan-2@outlook.com>
2024-04-06 02:52:22 +0800

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## 💌 Origin
+
+I have always been envious of the dynamic calendar icon in Wolai, but unfortunately, there are no related widgets in Obsidian. With the help of GPT, I developed a countdown widget.
+
+Due to my weak front-end skills and lack of familiarity with Obsidian's API calls, I managed to piece things together successfully, and I have to admit, I am quite impressed with myself.
+
+## ✨ Interface Preview
+
+![17123430204421712343019600.png](https://fastly.jsdelivr.net/gh/Achuan-2/PicBed@pic/assets/17123430204421712343019600.png)
+
+## 🐯 Main Features
+
+* Supports specifying a date for dynamic countdown and count-up calculations, relies on JavaScript code, does not depend on the network, and allows adding specific event names.
+  
+  * If the specified date is earlier than today's date, it counts up.
+  * If the specified date is later than today's date, it counts down.
+* Supports setting background color for some beautification.
+* Configuration supports automatic saving to block attributes in Obsidian, ensuring permanent retention in notes.
+* Supports exporting templates for loading configurations, facilitating repeated calls for specific countdown/count-up dates.
+
+## 🐛 Known Issues
+
+* The text is designed to be relatively large, so the widget block is not suitable if resized too small.
+* The Color Picker for setting colors does not support pasting colors.
+* To ensure that the widget and template can be saved and read block attributes correctly during the initial load, a delay of 500ms is added, resulting in slower display.
+
+> If anyone knows how to solve the above issues, please feel free to let me know. Your help is greatly appreciated.
+
+## ❤ Acknowledgements
+
+* ChatGPT and Google Gemini: Writing the main code.
+* [Zuoqiu-Yingyi/widget-query](https://github.com/Zuoqiu-Yingyi/widget-query): Learning how to set default width and height for widgets.
+* [OpaqueGlass/progressBarT-sywidget](https://github.com/OpaqueGlass/progressBarT-sywidget): Learning how widgets save and load configurations.
+
+## ☎️ Feedback
+
+Feel free to raise an issue on [GitHub](https://github.com/Achuan-2/siyuan-themes-tsundoku) or contact me via email.
---
+++ src/Calendar.js
@@ -0,0 +1,71 @@
+function updateCalendar() {
+        // 更新日历界面
+        const targetDateInput = document.getElementById('target-date');
+        const targetDate = new Date(targetDateInput.value);
+        const eventNameInput = document.getElementById('event-name');
+        const eventName = eventNameInput.value.trim(); // 获取事件名称并去除两端空格
+        const backgroundColorInput = document.getElementById('selected-color'); // 获取背景颜色选择器
+        const backgroundColor = backgroundColorInput.value; // 获取选择的背景颜色值
+        if (isNaN(targetDate.getTime())) {
+            alert('请输入有效的日期。');
+            return;
+        }
+
+        updateDate(targetDate);
+        // 显示事件
+        document.getElementById('calendar-event').textContent = eventName;
+    
+
+        const daysDiff = calculateCount(targetDate);
+        updateCountInfo(daysDiff, eventName);
+        document.getElementById('calendar-top').style = `background-color: ${backgroundColor};`;
+}
+
+
+
+function getToday() {
+    return new Date();
+}
+
+function formatDate(date) {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return { day, month, year };
+}
+
+function updateDate(date) {
+    const calendarDate = document.getElementById('calendar-date');
+    let date_object = new Date(date);
+    const { day, month, year } = formatDate(date_object);
+    const weekday = getWeekday(date_object);
+
+    calendarDate.innerHTML = `<span style="padding-left: 15px;">${year}.${month}.${day} ${weekday}</span>`;
+}
+
+function calculateCount(targetDate) {
+    const today = getToday();
+    const timeDiff = targetDate - today;
+    const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    return daysDiff;
+}
+
+function updateCountInfo(daysDiff, eventName) {
+    const countInfo = document.getElementById('count-info');
+    if (daysDiff >= 0) {
+        countInfo.textContent = `还有 ${daysDiff} 天`;
+    } else {
+        countInfo.textContent = `已过 ${Math.abs(daysDiff)} 天`;
+    }
+}
+
+function getWeekday(date) {
+    let date_object = new Date(date);
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return weekdays[date.getDay()];
+}
+
+function toggleSettingsMenu() {
+    const settingsMenu = document.getElementById('calendar-options');
+    settingsMenu.style.display = settingsMenu.style.display === 'flex' ? 'none' : 'flex';
+}
